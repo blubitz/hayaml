f98a0b755a5c4355906a7ac10255b56e069a51ee
blubitz <20598803+blubitz@users.noreply.github.com>
2025-10-19 00:11:32 +0800

    add umd support

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 export default {
-    input: './dist/esm/hayaml.mjs',
+    input: './src/cjs/hayaml.js',
     output: {
         file: 'dist/umd/hayaml.js',
         format: 'umd',
         name: 'HAYAML' // The global variable name for your library
-    }
+    },
+    plugins: [
+        commonjs(), // This plugin converts your CJS code into ESM
+        nodeResolve()
+    ]
 };
